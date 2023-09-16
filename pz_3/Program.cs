cf5c3ffe9011179f56c754127345e5b03d9d1536
IlyaE125 <ermolenko_0606@mail.ru>
2023-09-16 09:36:19 +0500

    пз 3

---
+++ pz_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.Design;
+
+namespace pz_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            int i = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"мне{i}");
+                if (i >= 11 && i <= 14) 
+                {
+                    Console.WriteLine("лет");
+                    Int32 lastnumber = i % 10;
+                }
+                else
+                {
+                    Int32 lastnumber = i % 10;
+                    switch (lastnumber)
+                    {
+                        
+                        case 1:
+                            Console.WriteLine("год");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            Console.WriteLine("года");
+                            break;
+                        default:
+                            Console.WriteLine("лет");
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
