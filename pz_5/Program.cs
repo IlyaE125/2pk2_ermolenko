c5ff4498c359ea4e128f45fa9abf1e720e012346
IlyaE125 <ermolenko_0606@mail.ru>
2023-09-23 09:40:13 +0500

    pz_5

---
+++ pz_5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());         
+            int m = (n % 2 == 0) ? n + 1 : n;
+            int upperLimit = m * 2;
+            int sum = 0;
+            int currentNumber = m;
+            while (currentNumber <= upperLimit)
+            {
+                if (currentNumber % 2 != 0)
+                {
+                    sum += currentNumber;
+                }
+                currentNumber++;
+            }
+            Console.WriteLine("Сумма всех нечетных чисел от " + m + " до " + upperLimit + ": " + sum);
+        }
+    }
+}
