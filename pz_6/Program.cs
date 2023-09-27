30e22736b27bc26f0fb67e572d0911d20bbfe05e
IlyaE125 <ermolenko_0606@mail.ru>
2023-09-27 22:19:16 +0500

    pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int[50];
+            for (int i = 0; i < 50 ; i++)
+            {
+                Array[i] = i;
+            }
+            Console.WriteLine(Array);
+            int num = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] >= 15)
+                {
+                    Array[num] = Array[i];
+                    num++;
+                }    
+            }
+            for (int i = num; i < Array.Length; i++)
+            {
+                Array[i] = 0;
+            }
+
+            Console.WriteLine("Массив после исключения элементов меньше 15:");
+            PrintArray(Array);
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (var number in arr)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
