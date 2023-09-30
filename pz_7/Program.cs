ebfa6a420e59577d0d292eaf139bfd89f7713849
IlyaE125 <ermolenko_0606@mail.ru>
2023-09-30 09:31:48 +0500

    pz_7

---
+++ pz_7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] arr = new int[5, 5];
+            Console.WriteLine(" массив arr: ");
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    arr[i, j] = Math.Abs(i - j) + 1;
+                    Console.Write(arr[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
