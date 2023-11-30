a54b507a271fb264317d1c392d88fd4863912879
IlyaE125 <ermolenko_0606@mail.ru>
2023-11-30 15:41:28 +0500

    pz_12

---
+++ pz_12/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {
+          static bool CheckFormula(string formula)
+          {
+                int count = 0;
+
+                foreach (char sym in formula)
+                {
+                    if (sym == '(')
+                        count++;
+                    else if (sym == ')')
+                        count--;
+
+                    // Если количество закрывающих скобок больше открывающих,
+                    // то формула некорректна
+                    if (count < 0)
+                        return false;
+                }
+
+                // Если количество открывающих и закрывающих скобок одинаково,
+                // то формула корректна
+                return count == 0;
+          }
+            static void Main(string[] args)
+            {
+                string formula = Console.ReadLine();
+                bool result = CheckFormula(formula);
+
+                Console.WriteLine(result);
+            }
+        
+    }
+}
