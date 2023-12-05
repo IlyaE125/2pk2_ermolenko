9c8b7d2315669292217aa4a5b63000c1602ad59e
IlyaE125 <ermolenko_0606@mail.ru>
2023-12-06 00:20:58 +0500

    pz_14

---
+++ pz_14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace pz_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+
+            // Создаем объект для записи данных в файл
+            StreamWriter writer = new StreamWriter("inFile.txt");
+
+            string input;
+            int num = 0;
+
+            do
+            {
+                // Считываем ввод пользователя
+                input = Console.ReadLine();
+
+                // Записываем ввод в файл
+                writer.WriteLine(input);
+
+                // Проверяем текст на наличие чисел
+                if (ContainsNumbers(input))
+                {
+                    num++;
+                }
+            } while (!string.IsNullOrEmpty(input));
+
+            writer.Close();
+
+            Console.WriteLine("Количество чисел в тексте: " + num);
+            Console.WriteLine("Содержимое файла inFile.txt:");
+            Console.WriteLine(File.ReadAllText("inFile.txt"));
+            Console.ReadLine();
+        }
+
+        static bool ContainsNumbers(string input)
+        {
+            // Проверяем, содержит ли текст числа
+            return Regex.IsMatch(input, @"\d");
+        }
+    }
+}
