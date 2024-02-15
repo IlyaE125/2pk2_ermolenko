e4f54a1fae121c3624f2172a78a995680ec9815b
IlyaE125 <ermolenko_0606@mail.ru>
2024-02-15 18:24:59 +0500

    pz18

---
+++ pz18/Product.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz18
+{
+    enum Category
+    {
+        Clothes,
+        Shoes,
+        Accessories
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+
+        private static int totalCount = 0;
+        private static decimal totalCost = 0;
+
+        public Product(string name, Category category, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Строка не может быть пустой", nameof(name));
+            }
+
+            if (price <= 0)
+            {
+                throw new ArgumentException("Цена не может быть ниже 0", nameof(price));
+            }
+
+            Name = name;
+            Category = category;
+            Price = price;
+
+            totalCount++;
+            totalCost += price;
+        }
+
+        public static void DisplayTotalInfo()
+        {
+            Console.WriteLine($"Общая стоимость продуктов: {totalCount}");
+            Console.WriteLine($"Полная стоимость продуктов: {totalCost}");
+            Console.WriteLine($"Средняя стоимость продуктов: {totalCost / totalCount}");
+        }
+
+        public void SellProduct()
+        {
+            decimal discount;
+            switch (Category)
+            {
+                case Category.Clothes:
+                    discount = 0.05m;
+                    break;
+                case Category.Shoes:
+                    discount = 0.07m;
+                    break;
+                case Category.Accessories:
+                    discount = 0.1m;
+                    break;
+                default:
+                    discount = 0;
+                    break;
+            }
+
+            decimal discountedPrice = Price - (Price * discount);
+
+            Console.WriteLine($"Продаваемый продукт {Name}");
+            Console.WriteLine($"Категория: {Category}");
+            Console.WriteLine($"Цена до скидки: {Price}");
+            Console.WriteLine($"Цена после скидки: {discountedPrice}");
+
+            totalCount--;
+            totalCost -= Price;
+        }
+    }
+}
---
+++ pz18/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace pz18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("T-shirt", Category.Clothes, 500);
+            Product product2 = new Product("Shoes", Category.Shoes, 1000);
+
+            
+            product1.SellProduct();
+            product2.SellProduct();
+
+            
+            Product.DisplayTotalInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
